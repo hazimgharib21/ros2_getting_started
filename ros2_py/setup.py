0ad26176b851014dee94e824b8c24da465c80b09
hazimgharib21 <hazimgharib21@gmail.com>
2022-08-20 23:20:00 +0800

    added minimal_publisher and minimal_subscriber

--- ros2_py/setup.py
+++ ros2_py/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = ros2_py.minimal_publisher:main',
+            'listener = ros2_py.minimal_subscriber:main',
         ],
     },
 )
